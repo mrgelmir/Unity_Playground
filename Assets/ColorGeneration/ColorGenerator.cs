c131ab2768fda26e815a75e766e294a247130fe4
Sander Decleer <sanderdecleer@gmail.com>
2017-10-27 21:54:07 +0200

    Initial commit

---
+++ Assets/ColorGeneration/ColorGenerator.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ColorGeneration
+{
+	public class ColorGenerator
+	{
+
+		// Static part
+
+		public static ICollection<Color> GetRandomColors(int amount)
+		{
+			ICollection<Color> col = new List<Color>();
+
+			for (int i = 0; i < amount; i++)
+			{
+				col.Add(GetRandomColor());
+			}
+
+			return col;
+		}
+
+		public static Color GetRandomColor()
+		{
+			return Random.ColorHSV(0f, 1f, 0f, 1f, 0f, 1f);
+		}
+
+		public static Color GetColor(Range hue, Range saturation, Range value)
+		{
+			return Random.ColorHSV(hue.Min, hue.Max, saturation.Min, saturation.Max, value.Min, value.Max);
+		}
+
+		public static Color GetColor(float hue, Range saturation, Range value)
+		{
+			return Random.ColorHSV(hue, hue, saturation.Min, saturation.Max, value.Min, value.Max);
+		}
+
+		// Instance part
+
+		private GeneratorSettings settings;
+
+		public GeneratorSettings Settings
+		{
+			get { return settings; }
+			set { settings = value; }
+		}
+
+		public ColorGenerator(GeneratorSettings settings)
+		{
+			this.Settings = settings;
+		}
+
+		public Color NextColor()
+		{
+			return GetColor(Range.Full, Settings.Saturation, Settings.Value);
+		}
+
+		public ICollection<Color> GetColorSet(int amount)
+		{
+			// Sanity checking
+			if(amount <= 0)
+			{
+				throw new System.ArgumentOutOfRangeException("amount", "Amount should be above zero");
+			}
+			if ((settings.MinAngle * (amount - 1)) > 1f)
+			{
+				throw new System.ArgumentException(string.Format("A minimum angle of {0} cannot be obtained with {1} elements", settings.MinAngle, amount));
+			}
+
+			List<Color> colors = new List<Color>(amount);
+
+			// Create first color
+			Color c = GetRandomColor();
+			colors.Add(c);
+
+			if (amount > 1)
+			{
+				// Get hue
+				float h, s, v;
+				Color.RGBToHSV(c, out h, out s, out v);
+
+				// Calculate hue variations using minimum angle, rotating right as a default
+				
+
+			}
+
+			return colors;
+		}
+
+
+		[System.Serializable]
+		public class GeneratorSettings
+		{
+			[Range(0f, .5f)]
+			public float MinAngle = 0f;
+			//[Range(.5f, 1f)]
+			//public float MaxAngle = 0f;
+			public Range Saturation = Range.Full;
+			public Range Value = Range.Full;
+		}
+
+		[System.Serializable]
+		public class Range
+		{
+			public static Range Full = new Range() { Min = 0, Max = 1 };
+			public static Range Create(float min, float max)
+			{
+				return new Range() { Min = min, Max = max };
+			}
+
+			public float Min;
+			public float Max;
+
+			public float GetValue()
+			{
+				return Random.Range(Min, Max);
+			}
+		}
+	}
+}
---
+++ Assets/ColorGeneration/Test/ColorTester.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ColorGeneration.Test
+{
+	public class ColorTester : MonoBehaviour
+	{
+		[SerializeField]
+		private RectTransform imageParent;
+
+		[SerializeField]
+		private int colorCount = 3;
+
+		[SerializeField]
+		private ColorGenerator.GeneratorSettings colorSettings;
+
+		private ColorGenerator cg;
+
+
+		public void GenerateColors()
+		{
+			// Clear images
+			for (int i = 0; i < imageParent.childCount; i++)
+			{
+				Destroy(imageParent.GetChild(i).gameObject);
+			}
+
+			// Generate new images
+			ICollection<Color> colors = cg.GetColorSet(3);
+			foreach (Color color in colors)
+			{
+				AddImage(color);
+			}
+		}
+
+		private void AddImage(Color color)
+		{
+			Image img = new GameObject().AddComponent<Image>();
+			img.color = color;
+			img.transform.SetParent(imageParent, false);
+		}
+
+	}
+}
